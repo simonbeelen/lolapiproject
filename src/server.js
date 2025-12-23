ca8692a96a3caab50b8f1d21efa29a65ea0b9688
simonbeelen <74742179+simonbeelen@users.noreply.github.com>
2025-12-23 13:41:02 +0100

    server.js en naamverandering voor gelijkenis tussen 2 routes

---
+++ src/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const db = require('./database');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+const championRoutes = require('./routes/champions');
+const itemRoutes = require('./routes/items');
+
+app.use('/api/champions', championRoutes);
+app.use('/api/items', itemRoutes);
+
+// Root route - API documentatie
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).json({ error: 'Endpoint not found' });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
+
+// Server starten
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
