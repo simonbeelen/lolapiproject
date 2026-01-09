8a6b00e602fce5cc13f7003d8796182430f4b0c0
simonbeelen <74742179+simonbeelen@users.noreply.github.com>
2026-01-09 16:31:57 +0100

    validatie routes volgens opdracht

--- src/routes/champions.js
+++ src/routes/champions.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const championController = require('../controllers/championController');
+const { validateChampion } = require('../middleware/validation');
 
 router.get('/', championController.getAllChampions);
+
 router.get('/:id', championController.getChampionById);
-router.post('/', championController.createChampion);
-router.put('/:id', championController.updateChampion);
+
+router.post('/', validateChampion, championController.createChampion);
+
+router.put('/:id', validateChampion, championController.updateChampion);
+
 router.delete('/:id', championController.deleteChampion);
 
 module.exports = router;
--- src/routes/items.js
+++ src/routes/items.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController');
+const { validateItem } = require('../middleware/validation');
 
 router.get('/', itemController.getAllItems);
+
 router.get('/:id', itemController.getItemById);
-router.post('/', itemController.createItem);
-router.put('/:id', itemController.updateItem);
+
+router.post('/', validateItem, itemController.createItem);
+
+router.put('/:id', validateItem, itemController.updateItem);
+
 router.delete('/:id', itemController.deleteItem);
 
 module.exports = router;
